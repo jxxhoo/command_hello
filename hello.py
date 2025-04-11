49bb92d3098099c36474041dcc487ded597a401c
jxxhoo <wlsgn0508@gmail.com>
2025-04-11 09:54:08 +0900

    github for git pull

--- hello.py
+++ hello.py
@@ -2,8 +2,8 @@
 // local repository modification
 // GotHub modification
 
-
 print("hello world")
 print("tell your world")
 print("tell his world")
 print("tell her world")
+print("tell my world")
